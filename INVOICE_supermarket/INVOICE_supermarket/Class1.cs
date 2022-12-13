6c006d47ccd2b11f6a07d0f7e7eb8e0034b03868
Mohamedasayed <120413155+Mohamedasayed@users.noreply.github.com>
2022-12-13 21:58:47 +0200

    Add files via upload

---
+++ INVOICE_supermarket/INVOICE_supermarket/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INVOICE_supermarket
+{
+    internal class Class1
+    {
+    }
+}
